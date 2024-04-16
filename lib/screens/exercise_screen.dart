8a1df543e4ea6bd1deb04d61afb2c591b8266f93
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2024-04-16 04:50:22 -0300

    :lipstick: Estilização tela de exercicio

--- lib/screens/exercise_screen.dart
+++ lib/screens/exercise_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:gym_helper_app/models/exercise.dart';
 import 'package:gym_helper_app/models/feeling.dart';
 
@@ -20,6 +22,7 @@ class ExerciseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightGreenAccent,
       appBar: AppBar(title: Text("${exercise.name} | ${exercise.training}")),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -27,33 +30,63 @@ class ExerciseScreen extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
-      body: Padding(
+      body: Container(
+        margin: EdgeInsets.all(8),
         padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+        ),
         child: ListView(
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("enviar foto"),
+            SizedBox(
+              height: 250,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("enviar foto"),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Tirar foto")
+                  ),
+                ],
+              ),
             ),
+            const SizedBox(height: 8),
             const Text("Como fazer?",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
             ),
+            const SizedBox(height: 8),
             Text(exercise.howTo),
-            Divider(),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Divider(color: Colors.black,),
+            ),
             const Text("Como estou me sentindo?",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
             ),
+            const SizedBox(height: 8),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: List.generate(feelingList.length, (index) {
                 Feeling feeling = feelingList[index];
-                return Text(feeling.feeling);
+                return ListTile(
+                  dense: true,
+                  contentPadding: EdgeInsets.zero,
+                  title: Text(feeling.feeling),
+                  subtitle: Text(feeling.date),
+                  leading: const Icon(Icons.double_arrow),
+                  trailing: IconButton(icon: const Icon(Icons.delete, color: Colors.redAccent), onPressed: () => print("delete"),),
+                );
               }),
             ),
           ],
